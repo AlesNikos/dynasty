156e6928285bc5479e497a23756014b4cb634b70
Alexey Kostyuchik <alesnikos@gmail.com>
2020-12-30 15:49:40 +0500

    Сделал страницу результатов поиска

--- build/script/script.js
+++ build/script/script.js
@@ -486,22 +486,25 @@ if(document.getElementById("map-2")){
     });
 
 
-    let servicesMenu = document.querySelector('.js-service-menu');
-    let servicesInfo = document.querySelector('.service__info-hero')
-    let blockPosition = window.pageYOffset + servicesMenu.getBoundingClientRect().top;
+    if(document.querySelector('.js-service-menu')) {
 
-    if (document.documentElement.clientWidth > 1365) {
-      window.addEventListener('scroll', function() {
-        let position = window.pageYOffset;
-        if (position > blockPosition - 96) {
-          servicesMenu.classList.add('service__menu--fixed');
-          servicesInfo.classList.add('service__info-hero--position');
-        } else {
-          servicesMenu.classList.remove('service__menu--fixed');
-          servicesInfo.classList.remove('service__info-hero--position');
-        }
-      });
-
+      let servicesMenu = document.querySelector('.js-service-menu');
+      let servicesInfo = document.querySelector('.service__info-hero')
+      let blockPosition = window.pageYOffset + servicesMenu.getBoundingClientRect().top;
+  
+      if (document.documentElement.clientWidth > 1365) {
+        window.addEventListener('scroll', function() {
+          let position = window.pageYOffset;
+          if (position > blockPosition - 96) {
+            servicesMenu.classList.add('service__menu--fixed');
+            servicesInfo.classList.add('service__info-hero--position');
+          } else {
+            servicesMenu.classList.remove('service__menu--fixed');
+            servicesInfo.classList.remove('service__info-hero--position');
+          }
+        });
+  
+      }
     }
     
 })();
